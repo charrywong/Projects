3b9c0c5d33c188b833212d4b5ab123824e09ed2b
charrywong <onechaoyang@gmail.com>
2015-10-30 16:10:18 +0800

    first test

---
+++ googleTranslate/myscript.js
@@ -0,0 +1,21 @@
+﻿function selectionOnClick(info, tab) { 
+if(funcChina(info.selectionText))
+	{
+		//不含有汉字
+		window.open('http://translate.google.cn/#en/zh-CN/'+info.selectionText);
+	}
+	else
+	{
+		//含有汉字
+		window.open('http://translate.google.cn/#zh-CN/en/'+info.selectionText);
+	}
+} 
+function funcChina(fanyistring){   
+	var obj =  fanyistring;
+	if(/.*[\u4e00-\u9fa5]+.*$/.test(obj)) {   
+    //alert("不能含有汉字！");   
+    return false;   
+   }   
+    return true;   
+}  
+var selection = chrome.contextMenus.create({"title": "谷歌翻译","contexts":["selection"],"onclick":selectionOnClick}); 
